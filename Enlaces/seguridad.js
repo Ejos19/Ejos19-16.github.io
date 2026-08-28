45c6516e30f7ada14c71a1149e02f1991075158a
Ejos19 <163593044+Ejos19@users.noreply.github.com>
2026-08-28 14:52:04 -0400

    Add new users to allowed users database

--- Enlaces/seguridad.js
+++ Enlaces/seguridad.js
@@ -9,6 +9,9 @@ document.addEventListener("DOMContentLoaded", () => {
     { usuario: "Emma.Mendez", pass: "Tealca2026*5" },
     { usuario: "Liseth.Gar", pass: "Tealca2026*4" },
     { usuario: "Andres.Ram", pass: "Tealca2026*3" },
+    { usuario: "Migleysi.Roy", pass: "V-18951095" },
+    { usuario: "Carmen.Del", pass: "V-19738393" },
+    
     // { usuario: "Admin.Ventas", pass: "Clave2026" }
   ];
 
